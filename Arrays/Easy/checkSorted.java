6dc6124cf3a920ba9ab67c657dbc4dc9e65fae9b
n3rddev <n3rddev.code@gmail.com>
2025-08-23 12:59:35 +0530

    Better code for checking an array if it is sorted

--- Arrays/Easy/checkSorted.java
+++ Arrays/Easy/checkSorted.java
@@ -2,19 +2,15 @@ package Arrays.Easy;
 
 public class checkSorted {
     public static boolean sorted(int nums[]){
-        boolean result=true;
-        for(int i = 1; i < nums.length; i++){
-            if(nums[i]<nums[i-1]){
-                result = false;
-                break;
-            }else{
-                result = true;
+        for(int i = 0; i< nums.length-1; i++){
+            if(nums[i] > nums[i+1]){
+                return false;
             }
         }
-        return result;
+        return true;
     }
     public static void main(String[] args) {
-        int nums[] = {3,3,6,7};
+        int nums[] = {3,3,6,7,3};
         System.out.print(sorted(nums));
     }
 }
