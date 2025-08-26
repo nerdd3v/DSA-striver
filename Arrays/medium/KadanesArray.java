a3b1da3dfcc8e63f6008111232fb07b3269ddce0
n3rddev <n3rddev.code@gmail.com>
2025-08-26 11:28:47 +0530

    DSA notes: Kadane's Algorithm Extended Version

--- Arrays/medium/KadanesArray.java
+++ Arrays/medium/KadanesArray.java
@@ -9,16 +9,20 @@ public class KadanesArray {
         
         for(int i = 0; i<nums.length; i++){
             sum+= nums[i];
+           
+            
             if(sum > max){
                 max = sum;
                 start = tempStart;
                 end = i;
             }
-
             if(sum<0){
                 sum = 0;
                 tempStart = i+1;
             }
+
+
+
         }
         int result[] = new int[end-start+1];
 
@@ -28,7 +32,7 @@ public class KadanesArray {
         return result;
     }
     public static void main(String[] args) {
-        int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        int[] nums = { -2, -3, -1, -5 };
         int[] result = kadanes(nums);
         System.out.print("Maximum sum subarray: ");
         for (int num : result) {
