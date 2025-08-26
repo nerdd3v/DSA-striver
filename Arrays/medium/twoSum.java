dac221ef8f02776f037ce2eaefba3de4ae59add3
n3rddev <n3rddev.code@gmail.com>
2025-08-26 09:47:46 +0530

    two sum -> 2 pointer approach -> condition: onli if allowed to sort (distort) the array

--- Arrays/medium/twoSum.java
+++ Arrays/medium/twoSum.java
@@ -1,23 +1,35 @@
 package Arrays.medium;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class twoSum {
     public static int[] two(int nums[], int target){
+        // Approach: only if sorting is allowed
+        
+        Arrays.sort(nums);
+
+        int right = nums.length-1;
+        int left = 0;
         int result[] = new int[2];
-        for(int i = 0; i< nums.length; i++){
-            for(int j = i+1; j<nums.length; j++){
-                if(nums[i]+nums[j] == target){
-                    result[0] = i;
-                    result[1] = j;
-                }
+        int notFOund[] = {-1};
+
+        while(left<=right){
+            if(nums[right]+nums[left] == target){
+                result[0] = left;
+                result[1] = right;
+                return result;
+            }else if(nums[right]+nums[left] < target){
+                left++;
+            }else{
+                right--;
             }
         }
-        return result;
+        return notFOund;
     }
     public static void main(String[] args) {
         int arr[] = {2,7,11,15};
-        int target = 17;
+        int target = 13;
         System.out.println(Arrays.toString(two(arr, target)));
     }
 }
