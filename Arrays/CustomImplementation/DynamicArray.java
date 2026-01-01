c41efc21504d99debca0b714d2cf0fc005953de0
n3rddev <n3rddev.code@gmail.com>
2026-01-01 23:55:23 +0530

    dynamic array done

--- Arrays/CustomImplementation/DynamicArray.java
+++ Arrays/CustomImplementation/DynamicArray.java
@@ -17,16 +17,45 @@ public class DynamicArray {
 
     public void add(int val){
         if(isFull()){
-            //call a function to increase the size of the array
+            arrayExtender();
+            // idx++;
+            // arr[idx] = val;
+            // return;
         }
+
         idx ++;
         arr[idx] = val;
     }
 
+    public int[] arrayExtender(){
+        int sizes = size*2;
+        this.size = sizes;
+        int data[] = new int[sizes];
+
+        for(int i = 0; i < arr.length; i++){
+            data[i] = this.arr[i];
+        }
+
+        this.arr = data;
+        return this.arr;
+    }
+
     protected boolean isFull(){
-        return this.size == idx;
+        return this.size-1 == idx;
     } 
+
+    public void print(){
+        for(int i = 0; i<= idx; i++){
+            System.out.println(arr[i]);
+        }
+    }
     public static void main(String[] args) {
-        
+        DynamicArray a = new DynamicArray(4);
+        a.add(1);
+        a.add(2);
+        a.add(3);
+        a.add(4);
+        a.add(5);
+        a.print();
     }
 }
